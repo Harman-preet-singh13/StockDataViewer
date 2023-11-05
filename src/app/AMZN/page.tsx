838b6a389e8d22b310e3efd9dc05aa17791960fa
Harmanpreet Singh <harmanp397@gmail.com>
2023-11-05 12:55:26 +0530

    landing page

---
+++ src/app/AMZN/page.tsx
@@ -0,0 +1,87 @@
+"use client";
+import axios from "axios";
+import { useEffect, useRef, useState } from "react";
+import ChartComponent from "../component/ChartComponent";
+import Link from "next/link";
+
+const API_KEY = "cl29t1pr01qinfqol3q0cl29t1pr01qinfqol3qg";
+
+const symbols =["GOOGL","AAPL","MSFT","TSLA","AMZN"]
+
+const getStockData = async (
+  symbol: string,
+  startTimestamp: number,
+  endTimestamp: number
+) => {
+  const response = await axios.get(
+    `https://finnhub.io/api/v1/stock/candle?symbol=${symbol}&resolution=D&from=${startTimestamp}&to=${endTimestamp}&token=${API_KEY}`
+  );
+  return response.data;
+};
+
+const StockData = () => {
+  const [stockData, setStockData] = useState(null);
+  const [symbol, setSymbol] = useState("AMZN")
+
+  useEffect(() => {
+    const fetchData = async () => {
+      
+      const startDate = "2022-10-04";
+      const endDate = "2023-11-04";
+
+      // Parse the input dates into UNIX timestamps
+      const startTimestamp = new Date(startDate).getTime() / 1000;
+      const endTimestamp = new Date(endDate).getTime() / 1000;
+
+      const data = await getStockData(symbol, startTimestamp, endTimestamp);
+      
+      const stockDataArray = data.c.map((closePrice: any, index: string | number) => ({
+        time: data.t[index],
+        open: data.o[index],
+        high: data.h[index],
+        low: data.l[index],
+        close: closePrice,
+        v: data.v[index],
+      }));
+      
+      setStockData(stockDataArray);
+      
+    };
+
+    
+    fetchData();
+
+  }, [symbol]);
+
+
+  if (!stockData) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+  <>
+  <div className="symbol-btn-container">
+  {symbols.map((symbol, index)=>{
+        return(
+          <Link
+          href={symbol}
+          key={index}
+          className=""
+          onClick={()=> setSymbol(symbol)}
+          >
+            {symbol}
+          </Link>
+        )
+      })}
+  </div>
+  <div className="lw-attribution">
+      <h1 className="text-2xl">
+        {symbol}
+      </h1>
+  </div>
+    <ChartComponent stockData={stockData} symbol={symbol} />
+  </>   
+  );
+};
+
+export default StockData;
---
+++ src/app/component/ChartComponent.tsx
@@ -0,0 +1,104 @@
+import React, { FC, useEffect, useRef, useState } from 'react';
+import { CandlestickData, CrosshairMode, IChartApi, createChart } from 'lightweight-charts';
+
+type ChartComponentProps = {
+  stockData: CandlestickData[];
+  symbol:string;
+};
+
+const ChartComponent: FC<ChartComponentProps> = ({ stockData, symbol }) => {
+  const [chartDimensions, setChartDimensions] = useState({
+    width: 0,
+    height: 0,
+  });
+
+  const chartRef = useRef<IChartApi | null>(null);
+
+  const resizeChart = () => {
+    const chartContainer = document.getElementById('chart-container');
+    if (chartContainer) {
+      const width = chartContainer.clientWidth;
+      const height = chartContainer.clientHeight;
+
+      setChartDimensions({
+        width,
+        height,
+      });
+    }
+  };
+
+  const currentLocale = window.navigator.languages[0];
+
+  const myPriceFormatter = Intl.NumberFormat(currentLocale, {
+    style: 'currency',
+    currency: 'USD',
+}).format;
+
+  useEffect(() => {
+    window.addEventListener('resize', resizeChart);
+
+    return () => {
+      window.removeEventListener('resize', resizeChart);
+    };
+  }, []);
+
+  useEffect(() => {
+    const chartContainer = document.getElementById('chart-container');
+    if (chartContainer) {
+      const chart = createChart(chartContainer, {
+        width: chartDimensions.width,
+        height: chartDimensions.height,
+      });
+
+      chartRef.current = chart;
+
+      const candlestickSeries = chart.addCandlestickSeries({
+        upColor: '#26a69a',
+        downColor: '#ef5350',
+        borderVisible: false,
+        wickUpColor: '#26a69a',
+        wickDownColor: '#ef5350',
+      });
+
+     
+      candlestickSeries.applyOptions({
+        priceFormat:{
+          type:'custom',
+          formatter:myPriceFormatter,
+        }
+      })
+
+      
+chart.applyOptions({
+  crosshair: {
+      
+      mode:CrosshairMode.Normal,
+      
+      vertLine: {
+          width:1,
+          color: '#000000',
+          style: 0,
+          labelBackgroundColor: '#000000',
+      },
+
+      
+      horzLine: {
+          color: '#000000',
+          labelBackgroundColor: '#000000',
+      },
+    }
+  })
+
+      console.log(stockData);
+
+      candlestickSeries.setData(stockData);
+    } else {
+      console.error('chartContainer element is not found');
+    }
+
+  }, [chartDimensions, stockData]);
+
+  return <div className="w-screen h-full mb-5" id="chart-container" />;
+};
+
+export default ChartComponent;
